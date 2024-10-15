a9f52c1b07219d213adbd0b813f97a1631eefae6
Hojoong Jang <hojoong310@gmail.com>
2024-10-15 19:08:22 +0900

    feat/post_detail

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('board_id', 'user_id', 'region_id', 'title', 'created_date')
+    search_fields = ('title', 'content')
--- community/views.py
+++ community/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Post
 
 def gangnam_view(request):
     return render(request, 'gangnam.html')
@@ -8,3 +10,12 @@ def seocho_view(request):
 
 def songpa_view(request):
     return render(request, 'songpa.html')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        board_id = self.kwargs.get('board_id')
+        return self.model.objects.get(board_id=board_id)  # post_id를 사용하여 객체 가져오기
