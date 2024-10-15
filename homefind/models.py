6d69590c9d3eac121ca7ce5aacddc444658818f6
coffee-isNaN <gjstjd9509@gmail.com>
2024-10-15 12:25:06 +0900

    feat/12
    1. django rest framework 추가
    2. homefind 앱 기초 구조 구현
    - models.py
    - urls.py
    - views.py

--- homefind/models.py
+++ homefind/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    region_name = models.CharField(max_length=100)
+
+
+class RealEstate(models.Model):
+    price = models.FloatField(default=0)
+    category = models.CharField(max_length=10)
+    pyeongsu = models.IntegerField(default=0)
+    house_name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Created Date')
+    spec_address = models.CharField(max_length=200)
+    region = models.ForeignKey(Region, related_name='real_estates', on_delete=models.CASCADE)
+    type = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.house_name
+
