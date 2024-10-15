d251cf8bce28e7a14a2f797f2ec0d9ccce9e01e4
박성준 <sungjunp1208@gmail.com>
2024-10-15 11:53:35 +0900

    settings add app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'community.apps.CommunityConfig',
+    'homefind.apps.HomefindConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
