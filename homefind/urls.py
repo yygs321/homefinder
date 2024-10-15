840869e42c3ee3d346883f2b325351326bd6d5a6
CoffeeIsNak <tjdqnzo@naver.com>
2024-10-15 16:55:34 +0900

    Update models(delete pyeongsu col and add rent_price col and change dtype of cols according to ERD) and views(add filter of rent_price)

--- homefind/urls.py
+++ homefind/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('mapdata/', MapData.as_view()),
     path('scountbygu/', ForSaleCountByGu.as_view()),
-    path('scountbytype/', ForSaleCountbyType.as_view()),
-    path('scountbycat/', ForSaleCountbyCategory.as_view()),
+    path('scountbytype/', ForSaleCountByType.as_view()),
+    path('scountbycat/', ForSaleCountByCategory.as_view()),
     path('wclouddata/', WordCloudData.as_view()),
     path('heatdata/', HeatMapData.as_view()),
 ]
