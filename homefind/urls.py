49c34988c03f59867a8c76e7b55a48ea12b28914
CoffeeIsNak <tjdqnzo@naver.com>
2024-10-15 13:29:51 +0900

    Ignore file delete

---
+++ homefind/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('mapdata/', MapData.as_view()),
+    path('scountbygu/', ForSaleCountByGu.as_view()),
+    path('scountbytype/', ForSaleCountbyType.as_view()),
+    path('scountbycat/', ForSaleCountbyCategory.as_view()),
+    path('wclouddata/', WordCloudData.as_view()),
+    path('heatdata/', HeatMapData.as_view()),
+]
