3a51ab0dce33e18381baa03de8c6ecd978f7fc91
HAKK <koreakseong22@gmail.com>
2024-10-16 11:03:49 +0900

    자치구별 상세 페이지추가

--- community/views.py
+++ community/views.py
@@ -19,3 +19,8 @@ class PostDetailView(DetailView):
     def get_object(self, queryset=None):
         board_id = self.kwargs.get('board_id')
         return self.model.objects.get(board_id=board_id)  # post_id를 사용하여 객체 가져오기
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # 자치구 이름을 추가로 전달
+        context['district_name'] = "강남구"  # 강남구 예시
