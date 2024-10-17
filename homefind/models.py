20557366c768a7f1f7cb01273a66d64edf6817cb
CoffeeIsNak <tjdqnzo@naver.com>
2024-10-17 10:10:02 +0900

    Update RealEstate model __str__

--- homefind/models.py
+++ homefind/models.py
@@ -52,4 +52,4 @@ class RealEstate(models.Model):
     )  # 건물 타입 (빌라, 원룸, 오피스텔)
 
     def __str__(self):
-        return self.region.region_name + '/' + self.house_name + '/' + self.type + '/' + self.category + '/' + '가격: ' + str(self.price)
+        return self.region.region_name + '/' + self.house_name + '/' + self.type + '/' + self.category + '/' + '가격: ' + str(self.price) + '/' + '생성일: ' + str(self.created_date)
