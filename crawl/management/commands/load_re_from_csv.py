9f777e5d47dd721e60bd738e7cfa5b34a20eb093
j-eum <eumjungmin1@gmail.com>
2024-10-17 14:05:19 +0900

    feat/ csv->db

--- crawl/management/commands/load_re_from_csv.py
+++ crawl/management/commands/load_re_from_csv.py
@@ -1,27 +1,22 @@
 import csv
 import os
 from django.core.management.base import BaseCommand
-from homefind.models import RealEstate
+from homefind.models import RealEstate, Region
 
 class Command(BaseCommand):
+    help = '지정된 폴더 내의 모든 CSV 파일을 읽어서 데이터베이스에 삽입합니다.'
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--folder',
-            type=str,
-            help='CSV 파일이 있는 폴더 경로를 지정합니다.',
-        )
+    folder_path = 'crawl/prop_crawler/property_data/'
 
-    def handle(self, *args, **options):
-        folder_path = options['folder']  # 명령어에서 받은 폴더 경로
-        if not folder_path:
-            self.stdout.write(self.style.ERROR('폴더 경로를 지정해 주세요.'))
+    def handle(self, *args, **kwargs):
+        if not os.path.exists(self.folder_path):
+            self.stdout.write(self.style.ERROR(f'{self.folder_path} 경로가 존재하지 않습니다.'))
             return
 
         # 폴더 경로에 있는 모든 파일을 순회
-        for filename in os.listdir(folder_path):
+        for filename in os.listdir(self.folder_path):
             if filename.endswith('.csv'):  # CSV 파일만 처리
-                file_path = os.path.join(folder_path, filename)
+                file_path = os.path.join(self.folder_path, filename)
                 self.stdout.write(self.style.NOTICE(f'{filename} 파일 처리 중...'))
 
                 # CSV 파일 읽기 및 데이터베이스에 삽입
@@ -29,7 +24,7 @@ class Command(BaseCommand):
                     reader = csv.DictReader(csvfile)
                     for row in reader:
                         RealEstate.objects.create(
-                            region_id=row['region_id'],
+                            region=Region.objects.get(pk=row['region_id']),
                             price=row['price'],
                             rent_price=row['rent_price'],
                             category=row['category'],
